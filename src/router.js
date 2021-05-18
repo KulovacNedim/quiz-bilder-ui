6c66ef7d77dee3c1978efec0f4a4e00f0fab16f9
Nedim Kulovac <nedim.kulovac@gmail.com>
2021-05-18 11:48:20 +0200

    code refactored: auth components styled and made responsive. base component for card and button created. button can serve as link too.

--- src/router.js
+++ src/router.js
@@ -1,10 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import UserAuth from './pages/auth/UserAuth';
+import SignInAndOut from './components/auth/SignInAndOut';
+import ForgotPassword from './components/auth/ForgotPassword';
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [{ path: '/auth', component: UserAuth }],
+  routes: [
+    {
+      path: '/auth',
+      component: UserAuth,
+      children: [
+        { path: '', component: SignInAndOut, name: 'signin' },
+        {
+          path: 'forgot-password',
+          component: ForgotPassword,
+          name: 'forgot-password',
+        },
+      ],
+    },
+  ],
 });
 
 export default router;
